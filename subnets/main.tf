235f52ff66386b72ede01d09d575a5ab940aca71
JanardhanReddy <bommireddy246@gmail.com>
2023-08-02 10:37:44 +0100

    terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -6,3 +6,21 @@ resource "aws_subnet" "main" {
 
   tags = merge({
     Name = "${var.env}-${var.subnet_name}-subnet"
+  },
+    var.tags)
+}
+
+resource "aws_route_table" "table" {
+  vpc_id = var.vpc_id
+
+  tags = merge({
+    Name = "${var.env}-${var.subnet_name}"
+  },
+    var.tags)
+}
+
+resource "aws_route_table_association" "association" {
+  count          = length(aws_subnet.main.*.id)
+  subnet_id      = element(aws_subnet.main.*.id, count.index)
+  route_table_id = aws_route_table.table.id
+}
--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
+variable "cidr_block" {}
 variable "tags" {}
 variable "env" {}
 variable "subnets" {}
 variable "az" {
   default = ["us-east-1a", "us-east-1b"]
 }
-variable "cidr_block" {}
+
 variable "default_vpc_id" {}
+variable "default_vpc_rt" {}
